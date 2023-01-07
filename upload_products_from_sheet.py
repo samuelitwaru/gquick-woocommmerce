fc4e75b135c7587656f5297c32f24bfc4d3788ff
samuelitwaru <samuelitwaru@gmail.com>
2023-01-07 19:39:39 +0300

    products

--- upload_products_from_sheet.py
+++ upload_products_from_sheet.py
@@ -1,34 +1,58 @@
 import pandas
+import numpy as np
 from product.create_product import create_product
 
 excel_data_df = pandas.read_excel(
-    'sheets/outdoors.xlsx', sheet_name='baskets')
+    'sheets/electronics.xlsx', sheet_name='blenders')
+
+
+excel_data_df = excel_data_df.replace({np.nan: None})
+
 
 # print whole sheet data
 data = excel_data_df.to_records()
 
-print(len(data))
 
 for row in data:
-    index, id, name, desc, price, img_1, img_2, addr = row
-    data = {
-        "name": name,
-        "type": "simple",
-        "regular_price": str(price),
-        "short_description": desc,
-        "categories": [
-            {
-                "id": 229
-            },
-            {
-                "id": 322
-            }
-        ],
-        "images": [
-            {
-                "src": img_1
-            }
-        ]
-    }
-    print(data)
-    create_product(data)
+    index = row[0]
+    id = row[1]
+    name = row[2]
+    desc = row[3]
+    price = row[4]
+    img_1 = row[5]
+    img_2 = row[6]
+    addr = row[7]
+
+    # print('>>>>>>>>>>>', row[8])
+
+    if not id:
+        data = {
+            "name": name,
+            "type": "simple",
+            "regular_price": str(price),
+            "short_description": desc,
+            "categories": [
+                {
+                    "id": 155
+                },
+                {
+                    "id": 353
+                },
+            ],
+            "images": [
+                {
+                    "src": img_1
+                }
+            ]
+        }
+
+        if isinstance(img_2, str):
+            data['images'].append(
+                {
+                    "src": img_2
+                }
+            )
+            # print(data['images'])
+
+        print(id)
+        create_product(data)
